fb8b792ecfd5c879152a6a84898a392cb1369696
Srinivasan Ramu <srinimarva@gmail.com>
2022-12-26 12:24:21 -0600

    Add files via upload

---
+++ src/main/java/com/dealerengine/www/DealerEngineApplication.java
@@ -0,0 +1,13 @@
+package com.dealerengine.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealerEngineApplication.class, args);
+	}
+
+}
